e7975afa4a0242293d029f7226c2f9daa133be13
declan kim <riverstack@naver.com>
2025-02-05 15:07:51 +0900

    rebase update

---
+++ pathParameters.py
@@ -0,0 +1,51 @@
+from fastapi import FastAPI
+from enum import Enum
+
+app = FastAPI()
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+
+@app.get("/items2/{item_id2}")
+async def read_item2(item_id2: int):
+    return {"item_id2": item_id2}
+
+
+# @app.get("/users/me")
+# async def read_user_me():
+#     return {"user_id": "the current user"}
+
+
+# @app.get("/users/{user_id}")
+# async def read_user(user_id: str):
+#     return {"user_id": user_id}
+
+
+@app.get("/users")
+async def read_users():
+    return ["Rick", "Morty"]
+
+
+@app.get("/users2")
+async def read_users2():
+    return ["Bean", "Elfo"]
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
